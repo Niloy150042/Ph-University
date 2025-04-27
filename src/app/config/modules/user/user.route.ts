644c5a81983227a85814a95097f703068d80e47b
Niloy150042 <mehedislam150042@gmail.com>
2025-04-27 19:26:15 +0600

    my army middleware is created

--- src/app/config/modules/user/user.route.ts
+++ src/app/config/modules/user/user.route.ts
@@ -3,13 +3,14 @@ import { usercontroller } from './user.controller';
 
 const userrouter = express.Router();
 
+const myarmymiddlware = (name) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    console.log(`khara ami shenabahini amar nam ${name}`);
+    next();
+  };
+};
 
-const myarmymiddlware =(req:Request,res:Response,next:NextFunction)=>{
-    console.log('khara ami shenabahini');
-    next()
-  
-}
-
-userrouter.post('/create-student', myarmymiddlware, usercontroller.createstudent);
+userrouter.post('/create-student',myarmymiddlware('validaterequst'),usercontroller.createstudent,
+);
 
 export default userrouter;
