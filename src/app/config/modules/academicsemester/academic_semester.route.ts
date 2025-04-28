93aa96b17ddae6a785781e28ad3acee02f118b8b
Niloy150042 <mehedislam150042@gmail.com>
2025-04-28 10:22:18 +0600

    academic semester validation

--- src/app/config/modules/academicsemester/academic_semester.route.ts
+++ src/app/config/modules/academicsemester/academic_semester.route.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import { semestercontroller } from './academic_semester.controller'
+import { academicvalidation } from './academic_semester_validation'
+import myarmymiddlware from '../utils/validationrequest'
 
 const semesterrouter = express.Router()
-semesterrouter.post('/create-academic-semester',semestercontroller.createacademicsemester)
+semesterrouter.post('/create-academic-semester', myarmymiddlware(academicvalidation.AcademicSemesterZodSchema),semestercontroller.createacademicsemester)
 
 export default semesterrouter
