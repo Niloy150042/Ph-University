2811db703cc523ba3852738a446df65a534c7882
Niloy150042 <mehedislam150042@gmail.com>
2025-04-28 11:50:28 +0600

    semester existing validation and semester code validation done .

--- src/app/config/modules/academicsemester/academic_semseter.model.ts
+++ src/app/config/modules/academicsemester/academic_semseter.model.ts
@@ -1,17 +1,16 @@
 import { model, Schema } from 'mongoose';
 import { Tacademic_semester } from './academic_semester.interface';
 
-
 const semesterschema = new Schema<Tacademic_semester>(
   {
     name: {
       type: String,
-      enum: ['Autum','Summer','Fall'],
+      enum: ['Autum', 'Summer', 'Fall'],
       required: true,
     },
     code: {
       type: String,
-      enum: ['01','02','03'],
+      enum: ['01', '02', '03'],
       required: true,
     },
     year: {
@@ -57,6 +56,24 @@ const semesterschema = new Schema<Tacademic_semester>(
     timestamps: true,
   },
 );
+
+semesterschema.pre('save', async function (next) {
+  const issemsterexist = await semestermodel.findOne({
+    name: this.name,
+    year: this.year,
+  });
+
+  if (issemsterexist) {
+    throw new Error('semester is already exist');
+  }
+  if (
+    (this.name == 'Autum' && this.code !== '01') ||
+    (this.name == 'Fall' && this.code !== '02') ||
+    (this.name == 'Summer' && this.code !== '03')
+  ) {
+    throw new Error('semester code is invalid');
+  }
+});
 
 export const semestermodel = model<Tacademic_semester>(
   'semester',
