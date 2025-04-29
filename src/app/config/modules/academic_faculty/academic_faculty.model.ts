da1eda892b4a0cee475df182de7253eb5a4ba9b3
Niloy150042 <mehedislam150042@gmail.com>
2025-04-30 00:57:05 +0600

    created academic faculty model .ts .

---
+++ src/app/config/modules/academic_faculty/academic_faculty.model.ts
@@ -0,0 +1,12 @@
+import { model, Schema } from 'mongoose';
+import { Tacademic_faculty } from './academic_faculty.interface';
+
+const academic_faculty_schema = new Schema<Tacademic_faculty>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+export const faculty_model = model<Tacademic_faculty>('academicfaculty',academic_faculty_schema)
