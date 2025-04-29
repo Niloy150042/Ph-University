6b71042ac2ebcdd13ab51a6888f91936407a9682
Niloy150042 <mehedislam150042@gmail.com>
2025-04-29 20:10:10 +0600

    fixing bugs.

--- src/app/config/modules/user/user.utils.ts
+++ src/app/config/modules/user/user.utils.ts
@@ -6,13 +6,24 @@ const laststudentid = async () => {
     .findOne({ role: 'student' })
     .lean()
     .sort({ createdAt: -1 });
+    //2030 01 0001
 
-  return laststudent?.id ? laststudent.id.slice(-4) : undefined;
+  return laststudent?.id ? laststudent.id :undefined;
 };
-
 export const generatestudentid = async (semester: Tacademic_semester) => {
-  const currentid = (await laststudentid()) || (0).toString();
-  let incrementid = (Number(currentid) + 1).toString().padStart(4, '0');
+
+  let currentid = (0).toString(); //0000
+  const lastenrlledstudentid = await  laststudentid() ||currentid
+  const laststudentsemestercode  =  lastenrlledstudentid.slice(4,6)
+  const lastsemesteryear =lastenrlledstudentid.slice(0,4)
+
+  if(semester.year===lastsemesteryear|| semester.code===laststudentsemestercode ){
+   currentid = lastenrlledstudentid.substring(6)
+
+  }
+  let incrementid = (Number(currentid) + 1).toString().padStart(4,'0');
   incrementid = `${semester.year}${semester.code}${incrementid}`;
   return incrementid;
+
+
 };
