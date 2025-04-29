6bc6f1bcb02157751cf3eba9ffb6c30b1fdf9f2e
Niloy150042 <mehedislam150042@gmail.com>
2025-04-29 21:17:28 +0600

    all logic of fixing bugs is done .

--- src/app/config/modules/user/user.utils.ts
+++ src/app/config/modules/user/user.utils.ts
@@ -6,24 +6,40 @@ const laststudentid = async () => {
     .findOne({ role: 'student' })
     .lean()
     .sort({ createdAt: -1 });
-    //2030 01 0001
+  //2030 01 0001
 
-  return laststudent?.id ? laststudent.id :undefined;
+  return laststudent?.id ? laststudent.id : undefined;
 };
 export const generatestudentid = async (semester: Tacademic_semester) => {
+  const  currentid = (0).toString().padStart(4,'0')//0000
+  const lastenrlledstudentid = await laststudentid();
 
-  let currentid = (0).toString(); //0000
-  const lastenrlledstudentid = await  laststudentid() ||currentid
-  const laststudentsemestercode  =  lastenrlledstudentid.slice(4,6)
-  const lastsemesteryear =lastenrlledstudentid.slice(0,4)
-
-  if(semester.year===lastsemesteryear|| semester.code===laststudentsemestercode ){
-   currentid = lastenrlledstudentid.substring(6)
+  if (lastenrlledstudentid) {
+    
+    const laststudentsemestercode = lastenrlledstudentid.slice(4, 6);
+    const lastsemesteryear = lastenrlledstudentid.slice(0, 4);
+    if (
+      semester.year == lastsemesteryear &&
+      semester.code == laststudentsemestercode
+    ) {
+      let incrementid = (Number(lastenrlledstudentid.slice(6,10)) + 1).toString().padStart(4, '0')
+     return  incrementid = `${semester.year}${semester.code}${incrementid}`;
+      
+    } 
+    else {
+    
+      const  incrementid = (Number(currentid) + 1).toString().padStart(4, '0');
+      // console.log( (`${semester.year}${semester.code}${incrementid}`)); 
+      return (`${semester.year}${semester.code}${incrementid}`) 
+  
+    }
+  }
+    else {
+    
+    const  incrementid = (Number(currentid) + 1).toString().padStart(4, '0');
+    // console.log( (`${semester.year}${semester.code}${incrementid}`)); 
+    return (`${semester.year}${semester.code}${incrementid}`) 
 
   }
-  let incrementid = (Number(currentid) + 1).toString().padStart(4,'0');
-  incrementid = `${semester.year}${semester.code}${incrementid}`;
-  return incrementid;
-
-
+ 
 };
