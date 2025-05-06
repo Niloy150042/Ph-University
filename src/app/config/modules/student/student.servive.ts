73457f331509abb5c6255ab5dedc7c874b3194d6
Niloy150042 <mehedislam150042@gmail.com>
2025-05-06 10:18:25 +0600

    sdf.

--- src/app/config/modules/student/student.servive.ts
+++ src/app/config/modules/student/student.servive.ts
@@ -4,14 +4,16 @@ const createstudentintodb = async (student:student)=>{
      const result = await studentmodel.create(student) // mongoose built in static method
      return result
 }
-
-const studentsearchablefield =['email', 'name.firstname','name.lastname','gender','avatar','bloodgroup','persentaddress']
 const getallstudentfromdb = async(query)=>{
+    const queryobj = {...query}
+
     let  searchterm =''
     if(query.SearchTerm){
         searchterm=query.SearchTerm
     }
-    const result = await studentmodel.find(
+  const studentsearchablefield =['email', 'name.firstname','name.lastname','gender','avatar','bloodgroup','persentaddress']
+
+    const searchquery =studentmodel.find(
 
         {
             $or:studentsearchablefield.map((field)=>({
@@ -19,7 +21,12 @@ const getallstudentfromdb = async(query)=>{
             }))
         }
 
-    ).populate('admissionsemester').populate({
+    )
+    const removefields = ['searchterm']
+    removefields.forEach((el)=>delete queryobj[el])
+    
+
+    const result = await searchquery.find(queryobj) .populate('admissionsemester').populate({
         path:'academicdepartment',
         populate:{
             path:'academic_faculty'
@@ -27,7 +34,6 @@ const getallstudentfromdb = async(query)=>{
     })
     return result
 }
-
 const getasinglestudent =async(id)=>{
     const result =await studentmodel.findOne({_id:id})
     return result
