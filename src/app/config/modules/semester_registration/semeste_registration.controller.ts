8a0df886856a872e5856e342f9839e634ee5091a
Niloy150042 <mehedislam150042@gmail.com>
2025-05-11 11:13:52 +0600

    done

--- src/app/config/modules/semester_registration/semeste_registration.controller.ts
+++ src/app/config/modules/semester_registration/semeste_registration.controller.ts
@@ -1,54 +1,63 @@
-import { Request, Response } from "express";
-import asynccatch from "../utils/catchasync";
-import { semesterregistraionservice } from "./semester_registration.service";
-import status from "http-status";
+import { Request, Response } from 'express';
+import asynccatch from '../utils/catchasync';
+import { semesterregistraionservice } from './semester_registration.service';
+import status from 'http-status';
 
-const createsemesterregistration = asynccatch(async(req:Request,res:Response,next)=>{
-    const result = await semesterregistraionservice.createsemesterregistrationintodb(req.body)
+const createsemesterregistration = asynccatch(
+  async (req: Request, res: Response, next) => {
+    const result =
+      await semesterregistraionservice.createsemesterregistrationintodb(
+        req.body,
+      );
     res.status(status.OK).send({
-        success:true,
-        message:'semester registration is successfull',
-        data:result
-    })
+      success: true,
+      message: 'semester registration is successfull',
+      data: result,
+    });
+  },
+);
 
-})
+const getallsemester = asynccatch(async (req: Request, res: Response, next) => {
+  const result = await semesterregistraionservice.getallregisteredsemester();
+  res.status(status.OK).send({
+    success: true,
+    message: 'all reagestered semester is retrived successfully',
+    data: result,
+  });
+});
 
-const getallsemester =asynccatch(async(req:Request,res:Response,next)=>{
-
-    const result = await semesterregistraionservice.getallregisteredsemester()
+const getsinglesemester = asynccatch(
+  async (req: Request, res: Response, next) => {
+    const id = req.params.id;
+    const result =
+      await semesterregistraionservice.getsingleregisteredsemester(id);
     res.status(status.OK).send({
-        success:true,
-        message:"all reagestered semester is retrived successfully",
-        data:result 
-    })
+      success: true,
+      message: 'your reagestered semester is retrived successfully',
+      data: result,
+    });
+  },
+);
 
-})
-
-
-const getsinglesemester =asynccatch(async(req:Request,res:Response,next)=>{
-    const id =req.params.id
-    const result = await semesterregistraionservice.getsingleregisteredsemester(id)
+const updateasinglesemester = asynccatch(
+  async (req: Request, res: Response, next) => {
+    const id = req.params.id;
+    const payload = req.body;
+    const result = await semesterregistraionservice.updateregesteredsemester(
+      id,
+      payload,
+    );
     res.status(status.OK).send({
-        success:true,
-        message:"your reagestered semester is retrived successfully",
-        data:result 
-    })
+      success: true,
+      message: 'your reagestered semester is updated successfully',
+      data: result,
+    });
+  },
+);
 
-})
-
-const updateasinglesemester =asynccatch(async(req:Request,res:Response,next)=>{
-    const id =req.params.id
-    const payload =req.body
-    const result = await semesterregistraionservice.updateregesteredsemester(id,payload)
-    res.status(status.OK).send({
-        success:true,
-        message:"your reagestered semester is updated successfully",
-        data:result 
-    })
-})
-
-export const semesterregistrationcontroller ={
-    createsemesterregistration,
-    getallsemester,getsinglesemester,
-    updateasinglesemester
-}
+export const semesterregistrationcontroller = {
+  createsemesterregistration,
+  getallsemester,
+  getsinglesemester,
+  updateasinglesemester,
+};
--- src/app/config/modules/semester_registration/semester_registration.service.ts
+++ src/app/config/modules/semester_registration/semester_registration.service.ts
@@ -1,73 +1,87 @@
-import { semestermodel } from "../academicsemester/academic_semseter.model"
-import { Tsemesterregistration } from "./semester_registration.interface"
-import semesterregistraionmodel from "./semester_registration.model"
+import { semestermodel } from '../academicsemester/academic_semseter.model';
+import { Tsemesterregistration } from './semester_registration.interface';
+import semesterregistraionmodel from './semester_registration.model';
 
-const createsemesterregistrationintodb = async(payload :Tsemesterregistration)=>{
-
-  const academicsemester = payload.academicsemester
+const createsemesterregistrationintodb = async (
+  payload: Tsemesterregistration,
+) => {
+  const academicsemester = payload.academicsemester;
   console.log(academicsemester);
 
   const isthereanyrunningsemester = await semesterregistraionmodel.findOne({
-    $or:[{status:"UPCOMING"},{status:"ONGOING"}]
-    
-  })
-  if(isthereanyrunningsemester){
-    throw new Error (` your ${isthereanyrunningsemester.status} semester is already running , you cant enter another semester registration untill its end `)
+    $or: [{ status: 'UPCOMING' }, { status: 'ONGOING' }],
+  });
+  if (isthereanyrunningsemester) {
+    throw new Error(
+      ` your ${isthereanyrunningsemester.status} semester is already running , you cant enter another semester registration untill its end `,
+    );
   }
 
   // checking is there semester named this objectid into db ?
-  const issemesterexist = await semestermodel.findOne({_id:academicsemester})
-  
-  if(!issemesterexist){
-    throw new Error ('there are no semester in this name')
+  const issemesterexist = await semestermodel.findOne({
+    _id: academicsemester,
+  });
+
+  if (!issemesterexist) {
+    throw new Error('there are no semester in this name');
   }
 
-  const issemesterregistered = await semesterregistraionmodel.findOne({academicsemester})
+  const issemesterregistered = await semesterregistraionmodel.findOne({
+    academicsemester,
+  });
 
-
-  if(issemesterregistered){
-    throw new Error ('this semester is already registered')
+  if (issemesterregistered) {
+    throw new Error('this semester is already registered');
+  } else {
+    const result = await semesterregistraionmodel.create(payload);
+    return result;
   }
-  else{
-    const result = await semesterregistraionmodel.create(payload)
-    return result 
-  }
-}
-const getallregisteredsemester = async()=>{
-    const result = await semesterregistraionmodel.find()
-    return result 
-}
+};
+const getallregisteredsemester = async () => {
+  const result = await semesterregistraionmodel.find();
+  return result;
+};
 
-const getsingleregisteredsemester = async(id)=>{
-    const result = await semesterregistraionmodel.find({_id:id})
-    return result 
-}
+const getsingleregisteredsemester = async id => {
+  const result = await semesterregistraionmodel.find({ _id: id });
+  return result;
+};
 
-const updateregesteredsemester = async(id:string,payload:Partial<Tsemesterregistration>)=>{
-  const istheresmesterexist = await semesterregistraionmodel.findById({_id:id})
-  if(!istheresmesterexist){
-    throw new Error ('there are no semester for this id into database ')
+const updateregesteredsemester = async (
+  id: string,
+  payload: Partial<Tsemesterregistration>,
+) => {
+  const istheresmesterexist = await semesterregistraionmodel.findById({
+    _id: id,
+  });
+  if (!istheresmesterexist) {
+    throw new Error('there are no semester for this id into database ');
   }
-  if(istheresmesterexist.status=='ENDED'){
-    throw new Error ('Your semester has been endeed , you cant update this ')
-  }
-
-  if(istheresmesterexist.status=="UPCOMING"&& payload.status=="ENDED"){
-    throw new Error (`You cant update your semester ${istheresmesterexist.status} to ${payload.status}`)
+  if (istheresmesterexist.status == 'ENDED') {
+    throw new Error('Your semester has been endeed , you cant update this ');
   }
 
-  if(istheresmesterexist.status == "ONGOING" && payload.status=='UPCOMING'){
-    throw new Error ( `You cant update your semester ${istheresmesterexist.status} to ${payload.status}`)
+  if (istheresmesterexist.status == 'UPCOMING' && payload.status == 'ENDED') {
+    throw new Error(
+      `You cant update your semester ${istheresmesterexist.status} to ${payload.status}`,
+    );
   }
-  
+  if (istheresmesterexist.status == 'ONGOING' && payload.status == 'UPCOMING') {
+    throw new Error(
+      `You cant update your semester ${istheresmesterexist.status} to ${payload.status}`,
+    );
+  }
 
-  const result = await semesterregistraionmodel.findOneAndUpdate({_id:id},payload,{new:true})
-  return result 
-}
-export const semesterregistraionservice ={
-    createsemesterregistrationintodb,
-    getallregisteredsemester,
-    getsingleregisteredsemester,
-    updateregesteredsemester
-    
-}
+  const result = await semesterregistraionmodel.findOneAndUpdate(
+    { _id: id },
+    payload,
+    { new: true },
+  );
+  return result;
+};
+export const semesterregistraionservice = {
+  createsemesterregistrationintodb,
+  getallregisteredsemester,
+  getsingleregisteredsemester,
+  updateregesteredsemester,
+};
