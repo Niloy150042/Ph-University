03dec5512daa0c4f8f43a856e58e632451d80439
Niloy150042 <mehedislam150042@gmail.com>
2025-05-19 21:25:11 +0600

    final done

--- src/app/config/modules/offered_course/offered_course.service.ts
+++ src/app/config/modules/offered_course/offered_course.service.ts
@@ -2,6 +2,12 @@ import { Tofferedcourse } from "./offered_course.interface"
 import { offerecoursemodel } from "./offered_course.model"
 
 const createofferedcouse = async(payload:Tofferedcourse)=>{
+//   checking if the admission semester is exist or not
+
+
+
+
+
     const result = await offerecoursemodel.create(payload)
     return result 
 }
