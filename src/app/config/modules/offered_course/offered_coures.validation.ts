a5b5d5ad55d55e94d571dca718c142d81ce6a333
Niloy150042 <mehedislam150042@gmail.com>
2025-05-24 11:34:38 +0600

    start and end time comparisong done

--- src/app/config/modules/offered_course/offered_coures.validation.ts
+++ src/app/config/modules/offered_course/offered_coures.validation.ts
@@ -13,13 +13,21 @@ export const offeredCourseZodSchema = z.object({
   days: daysEnum,
 
   startTime: z.string().min(1, 'Start time is required').refine((time)=>{
-    const regex =/^(0?[1-9]|1[0-2]):[0-5][0-9]\s?(AM|PM)$/i
+    const regex =/^(0?[1-9]|1[0-2]):[0-5][0-9]$/i
+
     return regex.test(time)
   },{message:"ivalid starttime format"}),
 
   endTime:  z.string().min(1, 'Start time is required').refine((time)=>{
-    const regex =/^(0?[1-9]|1[0-2]):[0-5][0-9]\s?(AM|PM)$/i
+    const regex =/^(0?[1-9]|1[0-2]):[0-5][0-9]$/i
+
     return regex.test(time)
   },{message:"ivalid endtime  format"})
 
+}).refine((body)=>{
+  const start = new Date(`1970-01-01T${body.startTime}:00`)
+  const end  = new Date(`1970-01-01T${body.endTime}:00`)
+  return end>start
+},{
+  message:"End time cannot held before starttime"
 }) ;
