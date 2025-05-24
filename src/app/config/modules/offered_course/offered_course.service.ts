65fa6f55dc8683a83f75118acda01083ddc63bc5
Niloy150042 <mehedislam150042@gmail.com>
2025-05-24 08:59:19 +0600

    semester existing checking done .

--- src/app/config/modules/offered_course/offered_course.service.ts
+++ src/app/config/modules/offered_course/offered_course.service.ts
@@ -1,13 +1,14 @@
+import { semestermodel } from "../academicsemester/academic_semseter.model"
 import { Tofferedcourse } from "./offered_course.interface"
 import { offerecoursemodel } from "./offered_course.model"
 
 const createofferedcouse = async(payload:Tofferedcourse)=>{
 //   checking if the admission semester is exist or not
-
-
-
-
-
+    const academiscsemesterexist =  await semestermodel.findById({_id:payload.academicsemester})
+    if(!academiscsemesterexist){
+        throw new Error(`your ${payload.academicsemester} doesnt exist on semester database`)
+    }
+    
     const result = await offerecoursemodel.create(payload)
     return result 
 }
