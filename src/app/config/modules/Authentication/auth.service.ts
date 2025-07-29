7ebad5b2a36eb1919a75fd037bb6043afaaf80e9
Niloy150042 <mehedislam150042@gmail.com>
2025-07-30 00:54:52 +0600

    creating jwt

--- src/app/config/modules/Authentication/auth.service.ts
+++ src/app/config/modules/Authentication/auth.service.ts
@@ -1,28 +1,44 @@
 import { user } from '../user.model';
 import { Tloginuser } from './auth.interface';
-import bcrypt from'bcrypt'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 
 const loginuser = async (payload: Tloginuser) => {
-  const isuserexist = await user
+  const User = await user
     .findOne({ id: payload?.id })
     .select(
       '_id id password needpasswordchange isdeleted role status createdAt updatedAt',
     );
-  console.log(isuserexist);
 
-  if (!isuserexist) {
+  if (!User) {
     throw new Error('this user is not found');
   }
 
- // cheching if the user is blocked 
+  // cheching if the user is blocked
 
- if(isuserexist?.status == 'blocked'){
-    throw new Error('this user is blocked')
- }
- // password comparison 
- const ispasswordmatched = await bcrypt.compare(payload?.password,isuserexist?.password)
- console.log(ispasswordmatched);
- return {};
+  if (User?.status == 'blocked') {
+    throw new Error('this user is blocked');
+  }
+  // password comparison
+  const ispasswordmatched = await bcrypt.compare(
+    payload?.password,
+    User?.password,
+  );
+  if (!ispasswordmatched) {
+    throw new Error('password does not matched ');
+  }
+
+  const jwtpayload ={
+    id:User.id,
+    userstatus :User.status
+
+  }
+  
+jwt.sign({
+  data: jwtpayload
+}, 'secret', { expiresIn: 60 * 60 });
+
+  return {};
 };
 
 export const authservice = {
