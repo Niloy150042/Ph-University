b7dda9a7f38cdab4e05cf4959bb38f5b9005a6f1
Niloy150042 <mehedislam150042@gmail.com>
2025-08-13 23:17:39 +0600

    all done

--- src/app.ts
+++ src/app.ts
@@ -8,26 +8,24 @@ import cors from 'cors';
 import globalErrorHandler from './app/config/modules/utils/globalerrohandler';
 import notfound from './app/config/modules/utils/notfound';
 import router from './app/config/Routes/routes';
-import cookieParser from 'cookie-parser'
-
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({origin:['http://localhost:5000']}));
-app.use(cookieParser())
+app.use(cors({ origin: ['http://localhost:5000'] }));
+app.use(cookieParser());
 
-app.use('/api/v1',router)
+app.use('/api/v1', router);
 
-app.get('/test', async(req:Request,res:Response)=>{
-
+app.get('/test', async (req: Request, res: Response) => {
   // const a = 10
   // res.send(`${a}`)
-})
+});
 
-app.get('/',(req,res)=>{
-  res.send('hello dev niloy ')
-})
+app.get('/', (req, res) => {
+  res.send('hello  backend-dev niloy .. ! welcome to the ph-universtiy backend application');
+});
 app.use(
   globalErrorHandler as (
     err: Error,
@@ -37,6 +35,6 @@ app.use(
   ) => void,
 );
 
-app.use(notfound as (req: express.Request, res: express.Response) =>void);
+app.use(notfound as (req: express.Request, res: express.Response) => void);
 
 export default app;
