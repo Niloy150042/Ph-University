e58f40245db8fe5bd7fe59f8abf694be93e2e6a1
Niloy150042 <mehedislam150042@gmail.com>
2025-08-18 21:28:41 +0600

    creating course according to syllabus

--- src/app/config/modules/courses/courses.route.ts
+++ src/app/config/modules/courses/courses.route.ts
@@ -4,28 +4,42 @@ import myarmymiddlware from '../utils/validationrequest';
 import updatevalidationschema, {
   facultyvalidationschema,
 } from './courses.validation';
+import auth from '../utils/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const courserouter = express.Router();
 courserouter.post(
   '/create-course',
+  auth(USER_ROLE.admin),
   myarmymiddlware(updatevalidationschema),
   coursecontroller.createcourse,
 );
-// jodi kono docutment na thake tahole first create koro then update koro 
+// jodi kono docutment na thake tahole first create koro then update koro
 courserouter.put(
   '/:courseID/assign-faculties',
+  auth(USER_ROLE.admin),
   myarmymiddlware(facultyvalidationschema),
   coursecontroller.assignfacultyandcourse,
 );
 courserouter.delete(
-    '/:courseID/delete-faculties',
-    coursecontroller.removefacultyfromcourse,
-  );
+  '/:courseID/delete-faculties',
+  auth(USER_ROLE.admin),
+  coursecontroller.removefacultyfromcourse,
+);
 
-courserouter.get('/get-courses', coursecontroller.findallcourse);
-courserouter.get('/get-single-course/:id', coursecontroller.getasinglecourse);
+courserouter.get(
+  '/get-courses',
+  auth(USER_ROLE.admin),
+  coursecontroller.findallcourse,
+);
+courserouter.get(
+  '/get-single-course/:id',
+  auth(USER_ROLE.admin),
+  coursecontroller.getasinglecourse,
+);
 courserouter.patch(
   '/update-course/:id',
+  auth(USER_ROLE.admin),
   myarmymiddlware(updatevalidationschema),
   coursecontroller.updatecourse,
 );
